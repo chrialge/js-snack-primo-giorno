2fecf91ea2bfdd08e8c23782920f61805bf0f7c6
chrialge <chrialge@gmail.com>
2024-02-16 15:12:32 +0100

    completed exercise

---
+++ assets/js/main.js
@@ -0,0 +1,49 @@
+console.log("ciao"); //string
+
+/* 
+esercizio 1: Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console. 
+*/
+
+const number = 100; //variabile con valore number
+console.log(number);//la console ci dara il valore della variabile
+
+/*
+esercizio 2: Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+*/
+
+let numberPi = 3.14;//variabile con valore number
+console.log(numberPi);//la console ci dara il valore della variabile
+
+/* 
+esercizio 2b: Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
+*/
+
+let numberpi = 3.1416;//variabile con valore number
+console.log(numberPi);//la console ci dara il valore della variabile
+
+// risposta: non va oltre le due cifre doppo la virgola.
+// soluzione: usare come valore una stringa.
+
+/*
+esercizio 2c:   + Dichiara una variabile chiamata radius e assegnale il valore 8.
+                + Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+                + Stampa il valore della variabile circle in console.
+ */
+
+const radius = 8;//variabile con valore number
+const circle = (radius * 2) * numberPi;//variabile con valore number con operazione
+console.log(circle);//la console ci dara il valore della variabile
+
+/*
+esercizio 3:    + Dichiara una variabile chiamata name e
+                + assegnale il valore "Carlo".
+                + Successivamente, modifica il valore della variabile in "Marco".
+                + Stampa il nuovo valore in console.
+
+*/
+
+let userName = "Carlo";//variabile con valore string
+console.log(userName);//la console ci dara il valore della variabile
+
+userName = "Marco";//variabile con valore string
+console.log(userName);//la console ci dara il valore della variabile
